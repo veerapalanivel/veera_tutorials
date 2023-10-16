9cc0e43f8a558ec72304b5d6d4e74cb32907e99b
veerapalanivel <148067778+veerapalanivel@users.noreply.github.com>
2023-10-16 21:34:54 +0530

    Update file1.php

--- file1.php
+++ file1.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>title1</title>
 </head>
 <body>
 <h1>WELCOME TO FILE 1</h1>
-    <h1>file 1</h1>
+    <h1>file 1111</h1>
 </body>
 </html>
